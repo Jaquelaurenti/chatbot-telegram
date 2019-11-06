937f76bf6b12e2274170395529da4b424d71a364
Jaqueline.Laurenti <jaquelinelaurenti@gmail.com>
2019-11-05 23:05:22 -0300

    #1

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class Main {
+    public static void main(String[] args){
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(new Bot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
