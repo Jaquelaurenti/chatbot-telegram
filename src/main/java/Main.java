55b98a45b7a7be5db66e674b03e2a94c1b9362b5
Jaqueline.Laurenti <jaquelinelaurenti@gmail.com>
2019-11-06 02:27:38 -0300

    #8

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,7 @@ import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.model.request.ChatAction;
 import com.pengrad.telegrambot.request.GetUpdates;
 import com.pengrad.telegrambot.request.SendChatAction;
+import com.pengrad.telegrambot.request.SendLocation;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.response.BaseResponse;
 import com.pengrad.telegrambot.response.GetUpdatesResponse;
@@ -15,7 +16,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        TelegramBot bot = TelegramBotAdapter.build("812219430:AAE8Pti6Jj0MgeLKVyK6ZEivaytq1FheNUE");
+        ConfigChatBot config = new ConfigChatBot();
+        TelegramBot bot = TelegramBotAdapter.build(config.getTokenId());
         GetUpdatesResponse updatesResponse;
         SendResponse sendResponse;
         //objeto responsável por gerenciar o envio de ações do chat
@@ -23,10 +25,8 @@ public class Main {
 
         //controle de off-set, isto é, a partir deste ID será lido as mensagens pendentes na fila
         int m=0;
-
-        //loop infinito pode ser alterado por algum timer de intervalo curto
+        /* loop infinito pode ser alterado por algum timer de intervalo curto */
         while (true){
-
             //executa comando no Telegram para obter as mensagens pendentes a partir de um off-set (limite inicial)
 
             updatesResponse =  bot.execute(new GetUpdates().limit(100).offset(m));
@@ -39,15 +39,15 @@ public class Main {
                 m = update.updateId()+1;
 
                 System.out.println("Mensagem recebida "+ update.message().text());
+                sendResponse = bot.execute(new SendMessage(update.message().chat().id(), "Bem vindos ao Chatbot " + config.getBotUsername()));
+
 
                 MessagesChatbot messagesChatbot = new MessagesChatbot();
 
                 //envio de "Escrevendo" antes de enviar a resposta
                 baseResponse = bot.execute(new SendChatAction(update.message().chat().id(), ChatAction.typing.name()));
-
-                sendResponse = bot.execute(new SendMessage(update.message().chat().id(),messagesChatbot.onUpdateReceived(update)));
-
                 if(baseResponse.isOk()){
+                    sendResponse = bot.execute(new SendMessage(update.message().chat().id(),messagesChatbot.onUpdateReceived(update)));
                     System.out.println("Resposta enviada") ;
                 }
             }
