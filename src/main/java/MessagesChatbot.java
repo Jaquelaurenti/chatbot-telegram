7bf6fe252ac73831532f743c7901c9cb3860abd4
Sergio Henrique P. Oliveira <serginho@linx.com.br>
2019-11-08 14:57:57 -0300

    Mais mensagens

--- src/main/java/MessagesChatbot.java
+++ src/main/java/MessagesChatbot.java
@@ -11,8 +11,20 @@ public class MessagesChatbot {
         if(message.contains("burro")){
             responseMessage ="Burro é vc seu trouxa";
         }
-        else if(message.contains("apostila")){
+        else if(message.startsWith("1")){
             responseMessage = "Informe a matéria que deseja consultar Apostilas";
+
+        }
+        else if(message.startsWith("2")){
+            responseMessage = "Tá preocupado com as notas né.";
+
+        }
+        else if(message.startsWith("3")){
+            responseMessage = "Váaarias reposições!";
+
+        }
+        else if(message.startsWith("4")){
+            responseMessage = "Vixi tem trabalho pacas pra fazer!";
 
         }
         else{
