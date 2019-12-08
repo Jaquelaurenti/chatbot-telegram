7ec1fe88fe3026b21d5b094208f9e3ca3b98f83d
Jaqueline.Laurenti <jaquelinelaurenti@gmail.com>
2019-12-08 19:39:41 -0300

    Criacao da classe de Entrega de Trabalhos

---
+++ src/main/java/br/fiap/chatbot/telegram/model/EntregaTrabalhos.java
@@ -0,0 +1,9 @@
+package br.fiap.chatbot.telegram.model;
+
+public class EntregaTrabalhos {
+    private  int Id;
+    private String Rm;
+    private String Materia;
+    private  String Documento;
+    
+}
